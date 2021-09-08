651e41afbe832435fdf59a47e6ac297e2a666cf9
jeppekorgaard <korg@itu.dk>
2021-09-08 23:51:12 +0200

    Assignment0.Tests/UnitTest1.cs

--- Assignment0.Tests/UnitTest1.cs
+++ Assignment0.Tests/UnitTest1.cs
@@ -6,9 +6,26 @@ namespace Assignment0.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Is_leap_year_if_divisible_by_4()
         {
-
+            bool exptected = true;
+            bool acutal = Program.isLeapYear(40);    
+            Assert.Equal(exptected, acutal);
+            
+        }
+        [Fact]
+        public void Is_not_leap_year_if_divisible_by_4_and_100()
+        {
+            bool exptected = false;
+            bool acutal = Program.isLeapYear(1900);
+            Assert.Equal(exptected, acutal);
+        }
+        [Fact]
+        public void Is_leap_year_if_divisible_by_400()
+        {
+            bool exptected = true;
+            bool acutal = Program.isLeapYear(2000);
+            Assert.Equal(exptected, acutal);
         }
     }
 }
