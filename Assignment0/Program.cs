eacb5f24cf87b2c7b90fcaf0d1ec51ca967311af
JeppeKorgaard98 <90270219+JeppeKorgaard98@users.noreply.github.com>
2021-09-09 11:59:56 +0200

    Implemented error handling. (#4)
    Co-authored-by: jeppekorgaard <korg@itu.dk>

--- Assignment0/Program.cs
+++ Assignment0/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Assignment0
 {
@@ -6,16 +7,42 @@ namespace Assignment0
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Writes something and press [Enter]");
-            int number = Convert.ToInt32(Console.ReadLine());
-            if (isLeapYear(number))
+            if (args.Length == 0)
             {
-                Console.WriteLine("yay");
+                Console.WriteLine("Writes something and press [Enter]");
             }
             else
             {
-                Console.WriteLine("nay");
+                Console.WriteLine(args[0]);
             }
+            {
+                try
+                {
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    if (number > 1582)
+                    {
+                        if (isLeapYear(number))
+                        {
+                            Console.WriteLine("yay");
+                        }
+                        else
+                        {
+                            Console.WriteLine("nay");
+                        }
+                    }
+                    else
+                    {
+                        string[] output = { "Number must be higher than 1582" };
+                        Program.Main(output);
+                    }
+                }
+                catch (Exception)
+                {
+                    string[] output = { "Enter a number, not a word" };
+                    Program.Main(output);
+                }
+            }
+
         }
         public static bool isLeapYear(int year)
         {
