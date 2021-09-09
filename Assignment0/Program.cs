599d8f910411892e5fbe22ae637b922907580b6c
jeppekorgaard <korg@itu.dk>
2021-09-09 12:55:16 +0200

    quick fix

--- Assignment0/Program.cs
+++ Assignment0/Program.cs
@@ -53,11 +53,11 @@ namespace Assignment0
                 if (year % 100 == 0)
                 {
                     isLeapYear = false;
+                    if (year % 400 == 0)
+                    {
+                        isLeapYear = true;
+                    }
                 }
-            }
-            if (year % 400 == 0)
-            {
-                isLeapYear = true;
             }
             return isLeapYear;
         }
